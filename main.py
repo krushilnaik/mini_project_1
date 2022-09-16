8a2dedd6402648bb0a3a3e847f952181c6fb959c
Krushil Naik <krushilnaik96@gmail.com>
2022-09-16 14:23:34 -0400

    Plot and save stock charts

--- main.py
+++ main.py
@@ -4,10 +4,38 @@
 
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # (5/5 points) Proper import of packages used.
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import date, timedelta
+
+today = date.today()
+ten_days_ago = today - timedelta(days=10)
+
 # (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
 # (10/10 points) Store this information in a list that you will convert to a ndarray in NumPy.
+
+data = yf.download(
+    tickers="aapl dis tsla crsr msft",
+    start=ten_days_ago.isoformat(),
+    end=today.isoformat(),
+)
+
+adj_close = data["Adj Close"]
+
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
+for ticker in adj_close:
+    # close any existing figures to avoid copying it over in our new PNG
+    plt.close()
+
+    # parse the price data
+    array = np.array([price for price in adj_close[ticker]])
+
+    # plot and save the graphs
+    plt.plot(array)
+    plt.savefig(f"./charts/{ticker}.png")
+
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
